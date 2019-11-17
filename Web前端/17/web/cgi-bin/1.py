f1005a3a58db9af03b54ad8fc90cc6749c3b1f47
Mr seyou <249399289@qq.com>
2019-11-17 22:47:56 +0800

    2019.11.17

---
+++ Web前端/17/web/cgi-bin/1.py
@@ -0,0 +1,20 @@
+import cgi,cgitb,json,time
+cgitb.enable()
+
+print("Content-Type: text/html")
+print()
+
+# 接受用户发送的数据
+fs = cgi.FieldStorage()
+# 数据重组
+inputs = {}
+for key in fs.keys():
+    inputs[key] = fs[key].value
+
+# 给用户返回数据
+# print(inputs)
+
+# time.sleep(3)
+
+# 转换成json格式的数据
+print(json.dumps(inputs))
